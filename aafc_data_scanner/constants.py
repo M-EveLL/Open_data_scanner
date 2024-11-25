bf91b35d62e15df1c92ba128207bf50929bfd303
M-EveLL <Marie-Eve.loyer-larocque@agr.gc.ca>
2024-11-25 14:22:53 -0500

    Update constants.py

--- aafc_data_scanner/constants.py
+++ aafc_data_scanner/constants.py
@@ -14,23 +14,6 @@ REGISTRY_RESOURCES_BASE_URL = \
 to format with its package/datasets id, along with the resource id
 """
 
-CATALOGUE_BASE_URL = 'https://data-catalogue-donnees.agr.gc.ca/api/3/action/'
-"""Base url to send API requests to AAFC Open Data Catalogue"""
-CATALOGUE_DATASETS_BASE_URL = \
-    'https://data-catalogue-donnees.agr.gc.ca/aafc-open-data/{}'
-"""Base url to open a dataset on the AAFC Open Data Catalogue, 
-to format with its id
-"""
-CATALOGUE_RESOURCES_BASE_URL = \
-    'https://data-catalogue-donnees.agr.gc.ca/aafc-open-data/{}/resource/{}'
-"""Base url to open a resource on the AAFC Open Data Catalogue, 
-to format with its package/datasets id, along with the resource id
-"""
-
-
-AAFC_ORG_ID = '2ABCCA59-6C57-4886-99E7-85EC6C719218'
-"""ID of the organization AAFC on the Open Registry"""
-
 DATASETS_COLS = [
     'id', 'title_en', 'title_fr', 'published', 'modified',
     'metadata_created', 'metadata_modified', 'num_resources', 
